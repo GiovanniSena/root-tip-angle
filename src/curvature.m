42112c7a299474574d122bf9ff36c374f93e1fbd
Felicia <felicia.burtscher@hotmail.com>
2018-07-23 16:44:09 +0100

    approach 2 plots

--- src/curvature.m
+++ src/curvature.m
@@ -2,9 +2,9 @@
 % 23.7. ~noon
 % Computes the curvature of the root tip
 
-%% APPROACH 1: TRIANGLE -- 3 POINTS
+%% APPROACH 1: TRIANGLE -- 3 POINTS %%%%% DOES NOT WORK WELL B/C OF NON-DIFFERNTIABILITY OF LINE
 
-% MY formula for curvature is that of a curve defined in terms of a parameter t 
+% My formula for curvature is that of a curve defined in terms of a parameter t 
 % in which x?, y?, x?, and y? all refer to derivatives with respect to that parameter. 
 % If I have only the x and y coordinates of points on a curve, that formula will 
 % not be very useful.
@@ -43,6 +43,20 @@ plot(K)
 
 %% APPROACH 2: INTERPOLATION -- 9 POINTS
 
+% Use skeleton/bwboundaries to get a list of the (x,y) boundary coordinates. 
+% Run around that border taking a section of the curve, say 9 elements, and fit 
+% a polynomial to it. Knowing the derivative of the curve model I used, and 
+% the coefficients polyfit() gave me, I can get the curvature at each point. 
+% Then I run around the list of (x,y) edge coordinates using a marker color 
+% that corresponds to the curvature. 
+
+% A large radius of curvature means a "flat" part of the curve while a small radius 
+% of curvature means "pointy" parts of the curve.
+% Whether it's positive or negative just says which side of the curve it's bending to. 
+% I could just take the absolute value of the curvature if I don't care which 
+% side it bends toward; however, we do care as the root bends positively
+% (clockwise).
+
 
 % I = imread('curv_circle.tif');
 % binaryImage = im2bw(I);
@@ -53,16 +67,16 @@ plot(K)
 % x = boundaries{1}(:, 2);
 % y = boundaries{1}(:, 1);
 windowSize = 10;
-halfWidth = floor(windowSize/2);   %half window size
+halfWidth = floor(windowSize/2);   % half window size
 curvatures = zeros(size(skelmatR(:,1)));
 %
 %k =10
-for k = halfWidth+1 : length(x) - halfWidth
+for k = halfWidth+1 : length(skelmatR(:,1)) - halfWidth
 	theseX = skelmatR(k-halfWidth:k+halfWidth,1);   % 19 element of x
-	theseY = skelmstR(k-halfWidth:k+halfWidth,2);   % 19 elemebt of y
-	% Get a fit.
+	theseY = skelmatR(k-halfWidth:k+halfWidth,2);   % 19 element of y
+	% get a fit
 	coefficients = polyfit(theseX, theseY, 2);
-	% Get the curvature
+	% get the curvature
 	curvatures(k) = coefficients(1);
 	xc(k) = skelmatR(k,1);
 	yc(k) = skelmatR(k,2);	
@@ -70,7 +84,7 @@ for k = halfWidth+1 : length(x) - halfWidth
 end
 clc
 
-% Get rid of ridiculous curvatures (straight line segments).
+% get rid of ridiculous curvatures (straight line segments)
 curvatures(abs(curvatures) > 20) = 0;
 
 %curvatures = flip(curvatures)
@@ -87,9 +101,9 @@ max_Cur = max(curvatures)
 CurCur = curvatures;
 for i = 1 :L
     for j = 1 : W
-        if CurCur (i,j) < 0  %negative values
+        if CurCur (i,j) < 0  % negative values
            CurCur (i,j) =  CurCur (i,j) +(abs(min_Cur))
-        elseif CurCur (i,j) > 0  %positive values
+        elseif CurCur (i,j) > 0  % positive values
             CurCur (i,j) =  CurCur (i,j) + 100
         end
     end
@@ -99,17 +113,17 @@ curvatures = CurCur;
 %%
 
 %%
-% Make up a colormap
+% make up a colormap
 minC = min(curvatures)
 maxC = max(curvatures)
 range = ceil(maxC - minC)
 myColorMap = jet(range);
-% Display the image again.
-imshow(binaryImage);
+% display the image again
+imshow(workskelR);
 hold on;
 %
 for k = halfWidth+1 : length(skelmatR(:,1)) - halfWidth
-	% Get the index in the color map.
+	% get the index in the color map
 	thisIndex = round(size(myColorMap, 1) * (curvatures(k) - minC) / range)
 	
     fprintf('For point #%d, the colormap index is %d\n', k, thisIndex);
@@ -119,7 +133,7 @@ for k = halfWidth+1 : length(skelmatR(:,1)) - halfWidth
 	if isnan(thisIndex)
 		thisIndex = 1;
 	end
-	% Extract out the RGB triplet for this particular row in the color map.
+	% extract out the RGB triplet for this particular row in the color map
 	thisColor = myColorMap(thisIndex, :);
 	plot(skelmatR(k,1), skelmatR(k,2), '.', 'MarkerSize', 25, 'Color', thisColor);
 end
